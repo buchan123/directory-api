b8606717515fd35533e131f9674ba2e90b82203e
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2021-11-07 14:06:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,14 @@ const app = express();
 app.use(cors());
 app.use("/api", auth);
 app.use("/api", user);
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: "false",
+    message: "Page not found",
+    error: {
+      statusCode: 404,
+      message: "You reached a route that is not defined on this server",
+    },
+  });
+});
 module.exports = app;
